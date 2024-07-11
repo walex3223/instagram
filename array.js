94e679f5236c63f9fda38929059142d567040da2
walex3223 <olajideadewale4real@gmail.com>
2024-07-11 11:37:33 -0700

    instagram commit

---
+++ array.js
@@ -0,0 +1,47 @@
+const arr=[2,3,4]
+function make(inp){
+
+return inp*2
+}
+const ans=arr.map(make)
+console.log(ans)
+
+
+const nam=[
+    
+    {
+    age:true,
+    
+    tre:"olajide"},
+    
+    {
+        age:false,
+    
+        tre:"tunde"},
+       
+        
+    {
+    
+        age:false,
+        tre:"adewale",}
+    ]
+    
+    
+    
+    const filt=["adewake","tunder"]
+
+const color=["red","black"]
+let orange=false
+color.forEach((cols)=>{
+    if (cols=="black"){
+        orange=true
+    }
+
+})
+console.log(orange)
+
+
+const outy=filt.forEach((name, index)=>{console.log( name=="adewake"? name="woir": name)
+
+})
+console.log(outy)
